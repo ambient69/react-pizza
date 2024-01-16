759b555eff4c8b6582365e21a6a06bb4685d5eb5
Kozlenko <kozlenkonikita@outlook.com>
2024-01-16 14:36:31 +0300

    pizzas from back-end

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,23 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 
-const Categories = () => {
-  const [activeIndex, setActiveIndex] = React.useState(0);
+function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
-  const onClickCategory = (index) => {
-    setActiveIndex(index);
-  };
+
   return (
-    <div className="categories">
+    <div className='categories'>
       <ul>
-        {categories.map((value, i) => (
-          <li onClick={() => onClickCategory(i)} className={activeIndex === i ? 'active' : ''}>
+        {categories.map((value, index) => (
+          <li key={index} onClick={() => setActiveIndex(index)} className={activeIndex === index ? 'active' : ''}>
             {value}
           </li>
         ))}
       </ul>
     </div>
   );
-};
+}
 
 export default Categories;
